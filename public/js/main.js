d02f83a7462328d05a5ce4c8635a2f744795846c
Elias <eliaskalan@yahoo.gr>
2022-12-20 17:42:46 +0100

    from api to index.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,52 @@
+//const { response } = require("express")
+
 // Put your client side JS code here
 console.log('Hello world')
-fetch("https://wiki-shop.onrender.com/categories")
+
+var obj = fetch("https://wiki-shop.onrender.com/categories")
 .then(res => {
     console.log(res.ok)
     console.log(res.status)
     console.log(res)
 }).catch(err => console.log("Error"))
+
+
+
+fetch("https://wiki-shop.onrender.com/categories")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+    
+const api_url = "https://wiki-shop.onrender.com/categories";
+
+async function getapi(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data);
+    // if (response){
+    //     hideloader();
+    // }
+    show(data);
+}
+getapi(api_url);
+// function hideloader()
+// {
+//     document.getElementById('loading').style.display="none";
+// }
+function show(data){
+    let tab = 
+    `<tr>
+          <th>id</th>
+          <th>title</th>
+          <th>img_url</th>
+         </tr>`;
+    for (let r of data) {
+            tab += `<tr> 
+            <td>${r.id} </td>
+            <td>${r.title}</td>
+            <td>${r.img_url}</td>           
+            </tr>`;
+        }
+        // Setting innerHTML as tab variable
+        document.getElementById("para").innerHTML = tab;
+}
+

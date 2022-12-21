7d02cf150c3534acd6b312be20d66bfea86cb287
Elias <eliaskalan@yahoo.gr>
2022-12-21 15:09:16 +0100

    temp renamed

--- public/js/main.js
+++ public/js/main.js
@@ -14,31 +14,15 @@ async function getapi(url){
     // if (response){
     //     hideloader();
     // }
-    temp(data);
+    category_template(data);
 }
 //getapi(api_url);
 // function hideloader()
 // {
 //     document.getElementById('loading').style.display="none";
 // }
-function show(data){
-    let tab = 
-    `<tr>
-          <th>id</th>
-          <th>title</th>
-          <th>img_url</th>
-         </tr>`;
-    for (let r of data) {
-            tab += `<tr> 
-            <td>${r.id} </td>
-            <td>${r.title}</td>
-            <td>${r.img_url}</td>           
-            </tr>`;
-        }
-        // Setting innerHTML as tab variable
-        document.getElementById("para").innerHTML = tab;
-}
-function temp(name){
+
+function category_template(name){
     // compile the template
     var template = Handlebars.compile(`<ul>{{#each doesWhat}}<li><a class="category" href="./categories/technology.html">{{title}}</a> </li>{{/each}}</ul>`);
     // execute the compiled template and print the output to the console

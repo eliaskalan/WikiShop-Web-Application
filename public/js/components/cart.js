5932de64b51922aabeae265cd7b0c797789fd654
Elias <eliaskalan@yahoo.gr>
2023-01-17 15:10:15 +0200

    cart layout fixed.
    Total cost is missing

--- public/js/components/cart.js
+++ public/js/components/cart.js
@@ -1,14 +1,38 @@
 const cardProduct = (username,sessionId) => {
-    document.getElementById('id02').style.display='block'
+    
     const template = Handlebars.compile(
         `{{#each list}}
-                <h2>{{title}}</h2>
-				<h2>{{cost}}€</h2>	
-		</section>
+        <div  class="cart-item">
+            <div class="about">
+                <h1 class="title">{{title}}</h1>
+            </div>
+            <div class="counter">
+                <div class="count">Quantity</div>
+                <div class="btn">{{quantity}}</div>
+            </div>
+            <div class="prices">
+                <div class="amount">{{cost}} €</div>
+                <div class="remove"><u>Remove</u></div>
+            </div>
+            
+        </div>	    
         {{/each}}`);
 getCardProduct(username,sessionId).then(data => {     
     console.log(data.cartItems);
-    console.log(template({ list: data.cartItems }));
+    
     document.getElementById('cart-items').innerHTML = template({ list: data.cartItems });
     })
+}
+const cartTotal = () =>
+{
+    const template = Handlebars.compile(
+        `{{#each list}}
+            <h2>Σύνολο: {{totalCost}}</h2>
+        {{/each}}`
+    )
+    getCardProduct(username,sessionId).then(data => {
+            console.log(data.totalCost);
+            console.log(template({ list: data.cartItems }));
+            document.getElementById('total').innerHTML = template({list: data.totalCost});
+        })
 }
